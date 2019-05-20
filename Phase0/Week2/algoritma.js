d73f62cfade0ac95bdca72cab66a215ec51e13ff
jackbloo <a.iqbal1404@gmail.com>
2019-05-20 14:57:01 +0700

    PR 1

---
+++ Phase0/Week2/algoritma.js
@@ -0,0 +1,89 @@
+// ALGORITMA
+// MINUM KOPI
+// 1. AMBIL GELAS
+// 2. MASAK AIR
+// 3. TUANG KOPI
+// 4. TUANG GULA
+// 5. TUANG AIR PANAS
+// 6. DIADUK
+// 7. DIMINUM
+
+// PERHITUNGAN MATEMATIKA 6*7=42
+
+// Algoritma
+// Declare
+// 1. Simpan angka 6
+// 2. Simpan angka 7
+// 3. Kalikan angka di poin 1 dan poin 2
+// 4. Tampilkan hasil dari angka tersebut
+
+// Psuedocode
+// STORE 'a' with number 6
+// STORE 'b' with number 7
+// STORE 'c' with any number value
+// MULTIPLY 'a' and 'b'
+// DISPLAY 'c'
+
+// BAHASA JS
+var a = 6;
+var b = 7;
+var c = 0;
+var string = '';
+var array = [];
+var object = {};
+c = a * b;
+console.log(c);
+
+// STORE 'sayaLapar' with true
+// STORE 'sayaPunyaUang' with true
+// IF 'sayalapar is equal with true AND 'sayaPunyaUang' is equal with true THEN
+//    DISPLAY 'saya makan'
+//    'sayaLapar' with false
+// ELSE IF 'sayaLapar' is equal with true AND 'sayaPunyaUang' is equal to false THEN
+//    DISPLAY 'tidur'
+// END IF
+
+var sayaLapar = true;
+var sayaPunyaUang = true;
+if (sayaLapar === true && sayaPunyaUang === true) {
+    console.log('saya makan');
+    sayaLapar = false;
+} else if (sayaLapar === true) {
+    console.log('saya tidur');
+};
+
+// Adip punya duit dua ratus ribu, ibunya ngga bolehin adip duit pulang kl ga dihabisin. Ada pedagang [ermen dia setiap kuli ngejual permen harganya dua puluh ribu
+
+// 1. Adip beli permen
+// 2. Adip beli permen
+// 3. Adip beli permen
+// 4. Adip beli permen
+// 5. Adip beli permen ...
+// 10. Adip beli permen 
+// 11. Adip pulang, dan ibunya senang
+
+// 1. Selama duit adip belum abis, adip membeli permen..
+// 2. Adip pulang, dan ibunya senang
+
+// STORE 'uangAdip' with 200000
+// STORE 'hargaperment with 20000
+// WHILE 'uangAdip' higher than 0 THEN
+//      DISPLAY 'Adip beli permen'
+//      SET 'uangAdip' to 'uangAdip' MINUS 'hargaPermen'
+// END WHILE
+// DISPLAY 'Adip pulang, dan ibunya senang'
+
+// Andi punya 20 pacar, dia akan beristirahat setelah 20 pacarnya di telpon
+// 1. Andi telpon pacar1
+// 2. Andi telpon pacar2
+// ....
+// 20. Andi telpon pacar20
+// 21. Andi cari pacar lagi
+
+// STORE 'pacarAndi' with 20
+// STORE 'i' with 0
+// FOR pacarAndi > 'i'
+//      DISPLAY 'Andi telpon pacar'
+//      INCREMENT 'i' by 1
+// END FOR
+// DISPLAY 'Andi cari pacar lagi'
